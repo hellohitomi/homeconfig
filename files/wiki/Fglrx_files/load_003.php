bfaed8ea274b17c430c3327d20ccab1431aee6e2
hellohitomi <hellohitomi@gmail.com>
2011-04-07 01:11:35 +0800

    add wiki

---
+++ files/wiki/Fglrx_files/load_003.php
@@ -0,0 +1,3 @@
+;mw.loader.state({"site":"ready"});
+
+/* cache key: wiki_en:resourceloader:filter:minify-js:4:e0d3cde0f7cf2f20ba05d85a2a94359f */
---
+++ files/wiki/Xorg_(简体中文)_files/index.php
@@ -0,0 +1,8 @@
+/* generated javascript */
+var skin = 'archlinux';
+var stylepath = 'https://d11xdyzr0div58.cloudfront.net/wiki-1.16.0/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Archlinux.js */
