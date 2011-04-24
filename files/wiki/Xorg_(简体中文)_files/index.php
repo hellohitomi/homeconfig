6f59dca19ea69123c369981873ad668f1153db0d
hellohitomi <hellohitomi@gmail.com>
2011-04-24 17:22:34 +0800

    DL1

--- files/wiki/Xorg_(简体中文)_files/index.php
+++
@@ -1,8 +0,0 @@
-/* generated javascript */
-var skin = 'archlinux';
-var stylepath = 'https://d11xdyzr0div58.cloudfront.net/wiki-1.16.0/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:Archlinux.js */
